d651e18c1368908102e6952f50d3b871e8f3f239
M. Manolakis <mmanolak@hawaii.edu>
2025-02-03 19:56:02 -1000

    updating the checking lines

--- exercise3/Manolakis3.R
+++ exercise3/Manolakis3.R
@@ -5,8 +5,8 @@
 #set working directory; checker for dependent directory
 if (Sys.info()['sysname'] == "Windows") {
   if (Sys.info()['nodename'] == "DegreeLaptop" || Sys.getenv('USERNAME') == "Degree Laptop") { 
-    setwd("C:/Users/Degree Laptop/Desktop/Spring 2025/3 - Econ 256 (Data Vis)/exercise3")
-  } else { 
+    setwd("C:/Users/Degree Laptop/Desktop/Spring 2025/3 - Econ 256 (Data Vis)/exercise3")} 
+  else if (Sys.info()['nodename'] == "Michael" || Sys.getenv('USERNAME') == "Michael") { 
     setwd("C:/Users/Michael/Desktop/Spring 2025/3 - Econ 256/R Studio Files/Econ256/exercise3")}
 } else if (Sys.info()['sysname'] == "Linux") {
   setwd("~/Desktop/R Files/Econ256")
