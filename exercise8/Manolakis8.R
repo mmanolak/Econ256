11f52bc6421823db37622b779e1a04e77eaeb2c1
M. Manolakis <mmanolak@hawaii.edu>
2025-03-03 16:05:07 -1000

    Uploading exercise8

--- exercise8/Manolakis8.R
+++ exercise8/Manolakis8.R
@@ -1,9 +1,11 @@
 #Exercise 8
 #Michael Manolakis - Econ 256 Data Vis - CRN 86179
-#27 Jan 2025
+#3 March 2025
 
 #Load necessary library
 library(tidyverse)
+library(sf)
+
 #set working directory; checker for dependent directory
 if (Sys.info()['sysname'] == "Windows") {
   if (Sys.info()['nodename'] == "DegreeLaptop" || Sys.getenv('USERNAME') == "Degree Laptop") { 
@@ -16,4 +18,32 @@ if (Sys.info()['sysname'] == "Windows") {
   setwd("~/Desktop/Spring 2025/3 - Econ 256")}
 #Last line above is for macOS
 
-print(getwd())
+lis16<-read_csv('listings_2016.csv')
+lis18<-read_csv('listings_2018.csv')
+oahu<-read_sf(dsn='oahu', layer='oahu')
+
+#Plot 2016 Air BnB Listings
+ggplot()+
+  geom_sf(data=oahu, fill="lightgray", color="black")+
+  geom_point(data=lis16, aes(x=longitude, y=latitude), color="#ed6464", size=1.5, alpha=0.5)+
+  theme_void()+
+  ggtitle("Air BnB Listings in Oahu (2016)")+
+  labs(caption="Data sources: US Census Bureau and Inside Airbnb")
+
+#Plot 2018 Air BnB Listings
+ggplot()+
+  geom_sf(data=oahu, fill="lightgray", color="black")+
+  geom_point(data=lis18, aes(x=longitude, y=latitude), color="#ed6464", size=1.5, alpha=0.5)+
+  theme_void()+
+  ggtitle("Air BnB Listings in Oahu (2018)") +
+  labs(caption="Data sources: US Census Bureau and Inside Airbnb")
+
+ggsave("Manolakis-map2016.png")
+ggsave("Manolakis-map2018.png")
+
+#Did the number of Air BnB listings on Oahu increase or decrease from 2016 to 
+#2018? What is one neighborhood where there was a significant change in the 
+#number of listings?
+
+#There was a visible overall increase in Air BnB listings in 2018 from 2016
+#Areas that had notable increases would be in central Oahu, and Ewa to Kapolei area. 
